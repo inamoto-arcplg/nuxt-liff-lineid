bf685a1486d360fc360d97cf21fddcf7e88f8648
m.inamoto <inamoto@arcplg.com>
2021-12-03 17:50:47 +0900

    add photo content

---
+++ pages/content1.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="container">
+    <p class="line-id">LINE ID：{{ lineId }}</p>
+    <p><img src="https://placehold.jp/300x300.png" alt="" /></p>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+      },
+      lineId: null,
+    }
+  },
+  mounted() {
+    if (!this.canUseLIFF()) {
+      return
+    }
+
+    window.liff.init((data) => {
+      this.lineId = data.context.userId || null
+    })
+  },
+  methods: {
+    canUseLIFF() {
+      return navigator.userAgent.includes('Line') && window.liff
+    },
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  padding: 20px;
+  min-height: 100vh;
+}
+
+.line-id {
+  margin-bottom: 30px;
+}
+
+.form > * {
+  margin-bottom: 10px;
+}
+</style>
